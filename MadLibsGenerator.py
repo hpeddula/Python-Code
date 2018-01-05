f271cabf148aa02448972ee31ba2e5d8d118cc13
Harsha <hpeddula@outlook.com>
2018-01-05 11:38:50 +0530

    Add files via upload

---
+++ MadLibsGenerator.py
@@ -0,0 +1,9 @@
+name = input("Please Enter your Name: ")
+age = int(input("Please Enter your Age: "))
+email = input("Please Enter your Email: ")
+op = """
+Hi My Name is {}\n
+My age is {}\n
+My Email is {}\n
+""".format(name,age,email)
+print(op)
